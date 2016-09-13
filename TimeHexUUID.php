6f503a6c433502352a266014e5a87bb01757cbde
Mitch Lusas <logins@mitchlusas.com>
2016-09-13 12:32:41 -0700

    Time Hex UUID upload

---
+++ TimeHexUUID.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * User: mitchlusas
+ * Date: 11/16/15
+ * Time: 1:07 PM
+ */
+
+class TimeHexUUID
+{
+
+    /**
+    * Array of Characters for Randomizer 
+    *
+    */
+    private static $characters = [
+        'a', 'b', 'c', 'd', 'e',
+        'f', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8',
+        '9'
+    ];
+
+    /**
+     * Create a UUID with epoch unix timestamp + random hex characters
+     *
+     * @param bool $return_string
+     *
+     * @return string
+     */
+    public static function createUUID(bool $return_string = false){
+
+        $serverID = "a";
+
+        $microTime = sprintf('%.4f', microtime(true))*10000; // 0987654321.12345 (10 + 5 = 15 digits)
+
+        $uuid = $microTime;
+        $uuid .= self::createRandomCharacterString(16);
+        $uuid = $uuid.$serverID;
+
+        switch(true){
+            case $return_string == true:
+                //Return 32 HEX
+                return $uuid;
+                break;
+            case $return_string == false:
+                //Return 16 Binary
+                return hex2bin($uuid);
+                break;
+            default:
+                //Return 16 Binary
+                return hex2bin($uuid);
+                break;
+        }
+    }
+
+    /**
+     * Check the object->id to see if the id is set.  If not, create a UUID for object->id.  Return the object.
+     *
+     * @param $object
+     *
+     * @return mixed
+     */
+    public static function checkId($object){
+        if(!isset($object->id)){
+            $object->id = UUID::createUUID(false);
+        }
+        return $object;
+    }
+}
